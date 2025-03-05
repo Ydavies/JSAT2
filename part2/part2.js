f873cff81954f27bd7d90e4b397c6da4f6b28431
Yasuhiro Davies <ydavies@student.holmesglen.edu.au>
2025-03-05 12:18:39 +1100

    Update part2.js
    adjusted minor punctuation and removed all spaces

--- part2/part2.js
+++ part2/part2.js
@@ -1,26 +1,24 @@
-
 const myMovie = {
-    'title' : 'The Longest Yard',
-    'year' : '1964'
-     };
-     console.log(myMovie);
-     
-    //  Add rating: 5 
-    myMovie.rating = '5';
-    console.log(myMovie);
+  title: "The Longest Yard",
+  year: "1964",
+};
+console.log(myMovie);
 
-    // Add in Summary: World War II movie about the Normandy landings
-    myMovie.summary = "World War II movie about the Normandy landings";
-    console.log(myMovie);
+//  Add rating: 5
+myMovie.rating = 5;
+console.log(myMovie);
 
-    //  Change the year to 1962 
-    myMovie.year = 1962;
-    console.log(myMovie);
-    // Change the rating to 4
-    myMovie.rating = 4;
-    console.log(myMovie);
+// Add in Summary: World War II movie about the Normandy landings
+myMovie.summary = "World War II movie about the Normandy landings";
+console.log(myMovie);
 
+//  Change the year to 1962
+myMovie.year = 1962;
+console.log(myMovie);
+// Change the rating to 4
+myMovie.rating = 4;
+console.log(myMovie);
 
-    // Remove the Summary property 
-    delete myMovie.summary;
-    console.log(myMovie);
+// Remove the Summary property
+delete myMovie.summary;
+console.log(myMovie);
