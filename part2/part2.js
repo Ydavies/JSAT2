8f1262875a88a52714044f5cbc5babb42dd4d9b7
Yasuhiro Davies <ydavies@student.holmesglen.edu.au>
2025-03-05 16:05:55 +1100

    Updating font and text

--- part2/part2.js
+++ part2/part2.js
@@ -1,21 +1,22 @@
-
 const myMovie = {
   title: "The Longest Yard",
   year: "1964",
 };
 console.log(myMovie);
 
-//  add rating: 5 and Summary: World War II movie about the Normandy landings
-
-myMovie.rating = "5";
+//  Add rating: 5
+myMovie.rating = 5;
 console.log(myMovie);
+
+
+// Add in Summary: World War II movie about the Normandy landings
 myMovie.summary = "World War II movie about the Normandy landings";
 console.log(myMovie);
 
-// Change the rating to 4 and change the year to 1962
-
+//  Change the year to 1962
 myMovie.year = 1962;
 console.log(myMovie);
+// Change the rating to 4
 myMovie.rating = 4;
 console.log(myMovie);
 
